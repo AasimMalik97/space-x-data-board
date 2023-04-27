138eb84c75bce2ed9bb2e36d895d48dfcc5ca020
Aasim Malik <malik1997aasim@gmail.com>
2023-04-27 22:43:06 +0530

    css changed and search function implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import NoData from './No data-amico.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
@@ -9,6 +8,9 @@ function App() {
 
   const [data,setData] = useState([]);
   const [filter, setFilter] = useState("all");
+  const [searchInput, setSearchInput] = useState("");
+  const [searchFlag, setSearchFlag] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
 
   useEffect(()=>{
     async function getData(){
@@ -59,19 +61,24 @@ function App() {
       }
     }
     getData();
-  },[filter]);
+  },[filter]); 
+
+  function SearchLogic(){
+    const temp = data.filter((item)=>item.mission_name.toLocaleLowerCase().includes(searchInput));
+    setSearchFlag(true);
+    setSearchResult(temp);
+  }
 
   return (
     <div className="">
-      <div className="flex justify-center gap-3 px-2 items-center shadow-lg shadow-black-500/60">
-        <img src={logo} alt="" className='h-20 w-20'/>
-        <h3 className='font-bold text-2xl text-indigo-400'> Space X Launch Data  </h3>
+      <div className="flex justify-center gap-3 px-2 py-4 items-center shadow-lg shadow-black-500/60">
+       <h3 className='font-bold text-2xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'> Space X Launch  </h3>
       </div>
       <div className='container mx-auto px-3 py-4'>
         <div className="flex flex-col md:flex-row justify-end gap-5 md:items-center">
           <div className=''>
-            <h5 className=''> Filter </h5>
-            <select className='border active:outline-none px-2 py-1' value={filter} onChange={(e)=>{setFilter(e.target.value);}}>
+            <h5 className='text-black-500 font-medium text-sm'> Filter </h5>
+            <select className='border active:outline-none px-2 py-1 w-full md:w-auto' value={filter} onChange={(e)=>{setFilter(e.target.value);setSearchFlag(false); setSearchResult([]); setSearchInput("");}}>
               <option className='' value="all"> All Launches </option>
               <option className='' value="past"> Past Launches </option>
               <option className='' value="upcoming"> Upcoming Launches </option>
@@ -80,26 +87,40 @@ function App() {
             </select>
           </div>    
           <div className=''>
-            <h5 className=''> Search </h5>
+            <h5 className='text-black-500 font-medium text-sm'> Search </h5>
             <div className='flex gap-2'>
-              <input className='border px-2 py-1' placeholder="Search" type="text"/>
-              <button className='bg-blue-600 text-white rounded-md px-4 py-1'> Search </button>
+              <input className='border px-2 py-1 w-full md:w-auto active:outline-none focus:outline-none' placeholder="Search" type="search" value={searchInput} onChange={(e)=>{ if(e.target.value.length>0){setSearchInput(e.target.value);} else{ setSearchInput(""); setSearchFlag(false);}}} />
+              <button className='bg-blue-600 text-white rounded-md px-4 py-1' onClick={()=>{SearchLogic()}} > Search </button>
             </div>
           </div>    
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-10">
-          {data?.length>0?
-            data.map((item,index)=>(
+          {searchFlag? 
+            searchResult?.length > 0 ?
+              searchResult.map((item,index)=>(
+                <div key={index}>
+                  <DataCard data={item}/>
+                </div>
+              ))
+              :
+              <>
+                <div className='flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-4'>
+                  <img src={NoData} alt="" className="h-[500px]"/>
+                </div>
+              </>
+            : 
+              data?.length > 0 ?
+                data.map((item,index)=>(
               <div key={index}>
                 <DataCard data={item}/>
               </div>
-            ))
-          :
-            <>
-              <div className='flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-4'>
-                <img src={NoData} alt="" className=""/>
-              </div>
-            </>
+                ))
+             :
+              <>
+                <div className='flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-4'>
+                  <img src={NoData} alt="" className="h-[500px]"/>
+                </div>
+              </>
           }
         </div>
       </div>
